3731e9bcab0c3e61fba2f145df00fdbdc9224903
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-19 12:24:44 -0400

    added db.js to service worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,8 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-512x512.png",
+  "/db.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";
